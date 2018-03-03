57b99f3d2a955190227f75d18b973f169f23771e
aarskog <magnaars@ntnu.stud.no>
2018-03-03 15:58:43 +0100

    update

---
+++ backend/request_funding.py
@@ -0,0 +1,25 @@
+import json
+
+def request_funding(user,funding):
+	#if user.permission >= funding.funding.oermission_type:
+	if 1:
+		transfer_money()
+		funding.status = 'accepted'
+
+	else:
+		funding.status = 'denied'
+	user.add_funding(funding)
+
+	msg = {'status':funding.status}
+	return json.dumps(msg)
+
+def return_pending(funding):
+	#d for dict
+	msg = {'status':'pending'}
+	#d['status'] = 'pending'
+#	user.status = 'pending'
+
+	return json.dumps(msg)
+
+def transfer_money():
+	t = 1
