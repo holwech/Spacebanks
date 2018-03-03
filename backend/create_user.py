d78331045e1aeddaf375cac7b4816b2f97c8f651
stianlaa <stian.laate@gmail.com>
2018-03-03 17:56:30 +0100

    added create_user with include to sqlite

--- backend/create_user.py
+++ backend/create_user.py
@@ -1,6 +1,19 @@
+import user
+import sqlite3
 
 '''
 Check database if the user exist and create new user if not
 funding_permission need to be chosen here
 '''
 def create_user(user_id):
+    conn = sqlite3.connect('SQLite_data/user_storage.db')
+    c = conn.cursor()
+
+    if c.fetchone() == None:
+        #fetch user data, and store the new user in database
+        c.execute("INSERT INTO users VALUES ('%s','1')" % str(user_id))
+        user = user.User(self,user_id, 1 ):
+    else:
+        user = c.execute('SELECT * FROM users WHERE userid = %s' % str(user_id))
+
+    return user
