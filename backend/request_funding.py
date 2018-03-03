88670669a4a2657b422ef073762b4eafdd91d4a5
Andreas Aarvold <andreasaarvold@dhcp-10-24-21-33.wlan.ntnu.no>
2018-03-03 17:53:25 +0100

    magnus gjorde en ok jobb

--- backend/request_funding.py
+++ backend/request_funding.py
@@ -1,24 +1,27 @@
 import json
+import time
 
-def request_funding(user,funding):
-	#if user.permission >= funding.funding.oermission_type:
+def request_funding(funding):
+	#if user.permission >= funding.funding.permission_type:
+
+	# Wait for 10 sec to simulate that the company is approving the request
+	time.sleep(10) 
+
 	if 1:
 		transfer_money()
-		funding.status = 'accepted'
+		funding.status = 'approved'
 
 	else:
 		funding.status = 'denied'
-	user.add_funding(funding)
-
+	
 	msg = {'status':funding.status}
 	return json.dumps(msg)
 
-def return_pending(funding):
-	#d for dict
+def return_pending(user, funding):
+	# Save status to db
 	funding.status = 'pending'
 	msg = {'status':funding.status}
-	#d['status'] = 'pending'
-#	user.status = 'pending'
+	user.add_funding(funding)
 
 	return json.dumps(msg)
 
