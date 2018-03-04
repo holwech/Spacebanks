25159bc4ed51321443e0d75feaedd2a0fa2c176f
Andreas Aarvold <andreasaarvold@dhcp-10-24-21-33.wlan.ntnu.no>
2018-03-04 12:04:07 +0100

    fhis

--- backend/server.py
+++ backend/server.py
@@ -55,8 +55,8 @@ class httpServer(BaseHTTPRequestHandler):
 		else:
 			content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
 			post_data = self.rfile.read(content_length)			 # <--- Gets the data itself
-			reader = codecs.getreader("utf-8")
-			data = json.loads(reader(post_data))
+			#reader = codecs.getreader("utf-8")
+			data = json.loads(post_data)
 			request_is_valid, resp = handler.handle_request(route, data, FUNDING_TYPES)
 
 
